d4014a4fc9554a6064a9a1c184cb31bc7d116474
munair <munair@gmail.com>
2021-09-07 12:49:12 +0800

    initializing

---
+++ deploy.bash
@@ -0,0 +1,30 @@
+#! /bin/bash
+#
+# script name: deploy.bash
+# script author: munair simpson
+# script created: 20210907
+# script purpose: deploy to gh-pages or S3 bucket
+
+read -p "Install node packages with the 'npm install' command? [yes]: " install
+read -p "Build? [yes]: " build
+read -p "Push to staging on gh-pages? (https enabled by default) [yes]: " staging
+read -p "Push to production on AWS S3? (https not enabled by default) [yes]: " production
+
+install=${install:-"yes"}
+build=${build:-"yes"}
+staging=${staging:-"yes"}
+production=${production:-"yes"}
+
+# step 1: install
+if [ $install == "yes" ]; then npm install ; fi
+
+# step 2: build
+if [ $build == "yes" ]; then npm run build ; fi
+if [ $build == "yes" ] && [ -d docs ]; then mv docs /tmp/docs-$(date +%Y%m%d%H%M%S) ; fi
+if [ $build == "yes" ] && [ ! -d docs ]; then mv build docs ; fi
+
+# step 3: push to GitHub Pages (**staging**)
+if [ $staging == "yes" ]; then git add --all && git commit -m "bash produced commit message" && git push ; fi
+
+# step 4: copy to S3 bucket (**production**)
+if [ $production == "yes" ]; then aws s3 sync . s3://react-www-development ; fi
---
+++ setup.bash
@@ -0,0 +1,52 @@
+#! /bin/bash
+#
+# script name: setup.bash
+# script author: munair simpson
+# script created: 20210907
+# script purpose: setup EC2 instance for some REACT development
+
+# step 0: disable/enable debugging by setting debug to "false" or "true".
+debug="false" && echo [$0] set debug mode to "$debug".
+
+# step 1: update the package list and install Yarn. this also installs Node. install unzip and install NPM last.
+if $debug ; then sudo apt -y update && sudo apt -y install unzip && sudo apt -y install npm && sudo apt -y install vim && sudo apt -y install awscli; fi
+sudo apt -y update > /dev/null 2>&1 && echo [$0] updated APT packages.
+sudo apt -y install unzip > /dev/null 2>&1 && echo [$0] installed unzip APT.
+sudo apt -y install npm > /dev/null 2>&1 && echo [$0] installed NPM APT.
+sudo apt -y install vim > /dev/null 2>&1 && echo [$0] installed vim APT.
+sudo apt -y install awscli > /dev/null 2>&1 && echo [$0] installed awscli APT.
+
+# step 2: install Yarn version 12.
+if $debug ; then sudo npm install --global yarn; fi
+sudo npm install --global yarn > /dev/null 2>&1 && echo [$0] installed Yarn.
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.profile
+nvm install 12
+nvm use 12
+
+# step 3: verify the installation of APTs and configure AWS Client.
+nodeversion=$(nodejs --version) && echo [$0] verified the installation of nodejs version $nodeversion.
+npmversion=$(npm --version) && echo [$0] verified the installation of npm version $npmversion.
+yarnversion=$(yarn --version) && echo [$0] verified the installation of yarn version $yarnversion.
+awsversion=$(aws --version) && echo [$0] verified the installation of aws version $awsversion.
+aws configure
+
+# step 4: npm up.
+# if $debug ; then npm install && npm run start ; fi
+# sudo npm install > /dev/null 2>&1 && echo [$0] npmed up.
+# npm run start
+#
+# [stuff should be compiled and hosted locally at this point]
+
+
+# additional/alternate instructions:
+# npm install react-router-dom [if required]
+# npm start
+# npm run build && aws s3 sync build/ s3://staging-usefulcoin-com [to build and host on aws]
+
+# yarn instructions:
+# if $debug ; then yarn && yarn start ; fi
+# yarn > /dev/null 2>&1 && echo [$0] yarned up.
+# yarn start
+
+
